5b9d1eddeb9ff7ae646096c30e9e2bc6f106c43b
Mark Kil <markkilcanarias@gmail.com>
2024-02-24 13:47:10 +0100

    attack is ready

--- main/Character.ts
+++ main/Character.ts
@@ -5,11 +5,22 @@ export class Character {
     private _isAlive: boolean
 
     constructor() {
-        this._health = 1000;
-        this._level = 1;
-        this._isAlive = true;
+        this._health = 1000
+        this._level = 1
+        this._isAlive = true
     }
 
+    public attack(character: Character) {
+        let attackStrength = 100
+
+        if (character.health <= attackStrength) {
+            character.health = 0
+            character.isAlive = false
+            return
+        }
+
+        character.health = character.health - 100
+    }
     
     public get health() : number {
         return this._health
@@ -25,5 +36,13 @@ export class Character {
 
     public set health(health: number) {
         this._health = health
+    }
+
+    public set level(level: number) {
+        this._level = level
+    }
+
+    public set isAlive(isAlive: boolean) {
+        this._isAlive = isAlive
     }
 }
